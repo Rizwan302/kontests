150b7b2f7ace7393c6bbacc6b67cb89ef18446e6
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-03-16 17:28:29 +0500

    Style Navbar

---
+++ src/commands/Cardsitems.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import 'Cardsitems.css'
+
+function Cardsitems() {
+  return (
+    <div className='row'>
+        <div class="card">
+            Magic Card
+        </div>
+    </div>
+  )
+}
+
+export default Cardsitems
